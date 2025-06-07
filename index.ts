fb37a7ed76c87addf07859cd934fb0076ad908ef
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2025-06-07 17:10:43 +0200

    pull

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("Hello")
+import { exit } from "node:process"
+import { simpleGit } from "simple-git"
+
+const git = simpleGit()
+
+const status = await git.status()
+if (!status.isClean()) {
+  console.log(
+    "Current branch has uncommited changes. Please commit them before trying to create a new release."
+  )
+  exit(1)
+}
+
+const currentBranch = status.current
+console.log(`Detected local brach: ${currentBranch}`)
+
+console.log("Updating current branch...")
+const a = await git.pull()
+console.log(a)
