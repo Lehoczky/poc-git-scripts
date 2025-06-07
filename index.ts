2b1fc1707075c67a77a84f78db999a43b2d34f31
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2025-06-07 17:30:47 +0200

    nicer logs

--- index.ts
+++ index.ts
@@ -1,29 +1,33 @@
 import { exit } from "node:process"
 import { simpleGit } from "simple-git"
+import c from "picocolors"
 
-// dummy
-// dummy
+const logger = {
+  log: (message: string) => console.log(`ℹ️  ${message}`),
+  success: (message: string) => console.log(`✅ ${message}`),
+  error: (message: string) => console.error(`❌ ${message}`),
+}
 
 const git = simpleGit()
 
 const status = await git.status()
 if (!status.isClean()) {
-  console.log(
+  logger.error(
     "Current branch has uncommited changes. Please commit them before trying to create a new release."
   )
   exit(1)
 }
 
 const currentBranch = status.current
-console.log(`Detected local brach: ${currentBranch}`)
+logger.log(`Detected local brach: ${c.blue(currentBranch)}`)
 
-console.log("Updating current branch...")
+logger.log("Updating current branch...")
 // TODO: error handling for conflict
 const pullResult = await git.pull()
 const changes = pullResult.summary.changes
 const isBranchUpToDate = changes === 0
 if (isBranchUpToDate) {
-  console.log("Branch is up to date")
+  logger.success("Branch is up to date")
 } else {
-  console.log(`Pulled ${changes} changes from remote.`)
+  logger.success(`Pulled ${c.blue(changes)} changes from remote.`)
 }
